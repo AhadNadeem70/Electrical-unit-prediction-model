a0e5d3bd2087c703c0fedd41ed6b8c7d0c5d12e8
AhadNadeem70 <syedahadnadeem@gmail.com>
2025-10-16 23:29:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+# Load model and features
+model = joblib.load("model/trained_model.pkl")
+feature_columns = joblib.load("model/feature_columns.pkl")
+
+app = FastAPI(title="LightGBM Energy Prediction API")
+
+# Define input format
+class EnergyInput(BaseModel):
+    frequency: int
+    voltagePhaseR: float
+    currentPhaseR: float
+    powerfactorPhaseR: float
+    realpowerphaseR: float
+    totalLineCurrent: float
+    totalRealPower: float
+    hour: int
+    day: int
+    month: int
+ 
+
+@app.get("/")
+def home():
+    return {"message": "LightGBM Energy Prediction API is running!"}
+
+@app.post("/predict")
+def predict(data: EnergyInput):
+    df = pd.DataFrame([data.dict()])[feature_columns]
+    prediction = model.predict(df)
+    return {"prediction": float(prediction[0])}
